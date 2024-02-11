d0e355bc0b400bd7c7f5a36cbbc8e858d345db6f
Resis_G <luisgerman.com@gmail.com>
2024-02-11 11:05:21 +0100

    creacion de la ventana de iniciar secion

--- app/src/main/java/es/resisg/prometeoapp3/MainActivity.java
+++ app/src/main/java/es/resisg/prometeoapp3/MainActivity.java
@@ -3,12 +3,25 @@ package es.resisg.prometeoapp3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    //Atributos
+    EditText edtUsuario,edtContrasena;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //relacionar atributos con la parte gráfica
+        edtUsuario =(EditText) findViewById(R.id.edtUsuario_main_layout);
+        edtContrasena =(EditText) findViewById(R.id.edtContrasena_main_layout);
+
+    }
+
+    //metodo de accesi
+    public void Aceder(View view){
+
     }
 }
