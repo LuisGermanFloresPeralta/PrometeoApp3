326e40691ae762f34ebd65e575b9459beb78dfee
Resis_G <luisgerman.com@gmail.com>
2024-05-03 13:41:03 +0200

    Notificaciones funcionando Correctamente

--- app/src/main/java/es/resisg/prometeoapp3/modelo/MiServicio.java
+++ app/src/main/java/es/resisg/prometeoapp3/modelo/MiServicio.java
@@ -2,22 +2,30 @@ package es.resisg.prometeoapp3.modelo;
 
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Handler;
 import android.os.IBinder;
-import android.widget.Toast;
 
 import androidx.core.app.NotificationCompat;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import es.resisg.prometeoapp3.R;
+import es.resisg.prometeoapp3.controlador.ConectadoActivity.ConectadoActivity;
+import es.resisg.prometeoapp3.controlador.MainActivity;
+import es.resisg.prometeoapp3.modelo.conexionHTTP.peticiones;
 
 public class MiServicio extends Service {
     private static final String CHANNEL_ID = "my_channel";
     private Handler handler;
     private Runnable runnable;
+    private GestionSesion gestionSesion;
+    private int NumeroNotificacionesAnteriores=0;
 
     @Override
     public void onCreate() {
@@ -26,7 +34,22 @@ public class MiServicio extends Service {
         runnable = new Runnable() {
             @Override
             public void run() {
-                Toast.makeText(MiServicio.this, "hola", Toast.LENGTH_SHORT).show();
+
+                //Recuperamos datos de la sesion(usuario y contraseña)
+                gestionSesion = new GestionSesion(MiServicio.this);
+
+                //Hacemos la peticion para recoger la cantidad de notificaciones que existen
+                List<String> notificacionesList = new peticiones("http://192.168.1.141/WEB/APP/appNotificaciones.php", String.valueOf(gestionSesion.getUsuario()), gestionSesion.getContrasena()).conseguirNotificaciones();
+                for (int i =0;i<notificacionesList.size();i++){
+                    if(notificacionesList.size()>NumeroNotificacionesAnteriores){
+                        createNotificationChannel();
+                        showNotification(notificacionesList.get(i),i);
+                    }
+                }
+
+                //añadimos la cantidad de notificaciones que existen en este momento
+                NumeroNotificacionesAnteriores=notificacionesList.size();
+
                 handler.postDelayed(this,60000);
             }
         };
@@ -58,18 +81,28 @@ public class MiServicio extends Service {
         }
     }
 
-    private void showNotification() {
+    private void showNotification(String notificacion, int i) {
+        // Crear un Intent para abrir la actividad principal de tu aplicación
+        Intent intent = new Intent(this, ConectadoActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
         NotificationCompat.Builder builder = new NotificationCompat.Builder(this, CHANNEL_ID)
-                .setSmallIcon(R.drawable.ic_addusuario)
-                .setContentTitle("Mi notificación")
-                .setContentText("Hola, esta es una notificación de prueba!")
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+                .setSmallIcon(R.drawable.ic_usuario_morado)
+                .setContentTitle(gestionSesion.getNombre()+" tiene nueva acticvidad")
+                .setContentText("Ver detalles")
+                .setStyle(new NotificationCompat.BigTextStyle().bigText(notificacion))
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(pendingIntent) // Establecer el PendingIntent
+                .setAutoCancel(true);
 
         NotificationManager notificationManager = (NotificationManager) getSystemService(Context.NOTIFICATION_SERVICE);
-        notificationManager.notify(1, builder.build());
+        notificationManager.notify(i, builder.build());
     }
 
-   /* @Override
+
+
+    /* @Override
     public void onDestroy() {
         super.onDestroy();
         // Detiene la ejecución del Runnable cuando el servicio es destruido
